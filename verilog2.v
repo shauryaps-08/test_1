0e4f7141898bd68159125315dd3a89b74cc202b2
shauryaps_08 <shauryapratapsingh060208@gmail.com>
2026-06-23 05:20:05 +0000

    added verilo2

---
+++ verilog2.v
@@ -0,0 +1,17 @@
+module mux_4_to_1 (
+    input wire [3:0] in,     // 4 data inputs
+    input wire [1:0] sel,    // 2 select lines
+    output reg out           // Output
+);
+
+    always @(in or sel) begin
+        case (sel)
+            2'b00: out = in[0];
+            2'b01: out = in[1];
+            2'b10: out = in[2];
+            2'b11: out = in[3];
+            default: out = 1'bx; // Unknown output for safety
+        endcase
+    end
+
+endmodule
